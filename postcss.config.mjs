eee53e4cfbd18795786972df6c74c7a43878bf34
Michael-J-Romero <michaelromero93@yahoo.com>
2025-07-29 15:30:26 -0700

    5

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  // ["@tailwindcss/postcss"],
+  plugins: {
+      tailwindcss: {},
+    autoprefixer: {},
+  }
 };
 
 export default config;
